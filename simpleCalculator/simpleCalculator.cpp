6fa17aff61ad1e239bc691fa36d6aba7bf9177e9
Maria-Ashraf-Haleem <mariaashraf2004@gmail.com>
2025-02-04 00:47:44 +0200

    git push origin master

--- simpleCalculator/simpleCalculator.cpp
+++ simpleCalculator/simpleCalculator.cpp
@@ -49,158 +49,170 @@ void showAnswer(double answer)
 
 int main()
 {
-	double n1 = 0.0;
-	double n2 = 0.0;
-	double answer = 0.0;
-	char op = '\n';
-	bool again = 0;
-	while (op != '.')
+	cout << "Do you want any Calculator ? \n\n";
+	cout << "1- Scientific Calculator (Press \"1\")\n2- Programmer Calculator (Press \"2\")\n\n";
+	int type;
+	cin >> type;
+	system("cls");
+	if (type == 1)
 	{
-		if (again == 1)
+		double n1 = 0.0;
+		double n2 = 0.0;
+		double answer = 0.0;
+		char op = '\n';
+		bool again = 0;
+		while (op != '.')
 		{
-
-			showAnswer(answer);
-			cout << "Enter an Operation to perform: \n";
-			while (true)
+			if (again == 1)
 			{
-				cin >> op;
-				if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm' || op == 'l' ||
-					op == 'S' || op == 'C' || op == 'P' || op == 'r' || op == 'k' || op == 't' ||
-					op == 's' || op == 'c' || op == '.')
+
+				showAnswer(answer);
+				cout << "Enter an Operation to perform: \n";
+				while (true)
 				{
+					cin >> op;
+					if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm' || op == 'l' ||
+						op == 'S' || op == 'C' || op == 'P' || op == 'r' || op == 'k' || op == 't' ||
+						op == 's' || op == 'c' || op == '.')
+					{
+						break;
+					}
+					else
+					{
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+						cout << "Please Enter a Valid operation like + , -, *, / , m, s, etc ...\n";
+					}
+				}
+				if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm')
+				{
+					n1 = answer;
+					cout << "Enter your Number: \n";
+					cin >> n2;
+				}
+				else if (op == 'k' || op == '.')
+				{
+					// Don not take any thing
+
+				}
+				else
+				{
+					n1 = answer;
+				}
+				system("cls"); ////////////////////////
+			}
+			else
+			{
+				showCalculator();
+				cout << "Enter an Operation to perform: \n";
+				while (true)
+				{
+					cin >> op;
+					if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm' || op == 'l' ||
+						op == 'S' || op == 'C' || op == 'P' || op == 'r' || op == 'k' || op == 't' ||
+						op == 's' || op == 'c' || op == '.')
+					{
+						break;
+					}
+					else
+					{
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+						cout << "Please Enter a Valid operation like + , -, *, / , m, s, etc ...\n";
+					}
+				}
+
+				if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm')
+				{
+					cout << "Enter the first number: \n";
+					cin >> n1;
+					cout << "Enter the Seconed number: \n";
+					cin >> n2;
+				}
+				else if (op == 'k' || op == '.')
+				{
+					// Don not take any thing
+
+				}
+				else
+				{
+					cout << "Enter your Number: \n";
+					cin >> n1;
+				}
+				again = 1;
+				system("cls"); /////////////////////////////////////
+			}
+			switch (op)
+			{
+			case'+':
+				answer = n1 + n2;
+				break;
+			case'-':
+				answer = n1 - n2;
+				break;
+			case'*':
+				answer = n1 * n2;
+				break;
+			case'/':
+				if (n2 == 0)
+				{
+					cout << "Cannot Divide by Zero Bro !!!\n";
 					break;
 				}
 				else
 				{
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
-					cout << "Please Enter a Valid operation like + , -, *, / , m, s, etc ...\n";
-				}
-			}
-			if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm')
-			{
-				n1 = answer;
-				cout << "Enter your Number: \n";
-				cin >> n2;
-			}
-			else if (op == 'k' || op == '.')
-			{
-				// Don not take any thing
-
-			}
-			else
-			{
-				n1 = answer;
-			}
-			system("cls"); ////////////////////////
-		}
-		else
-		{
-			showCalculator();
-			cout << "Enter an Operation to perform: \n";
-			while (true)
-			{
-				cin >> op;
-				if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm' || op == 'l' ||
-					op == 'S' || op == 'C' || op == 'P' || op == 'r' || op == 'k' || op == 't' ||
-					op == 's' || op == 'c' || op == '.')
-				{
+					answer = n1 / n2;
 					break;
 				}
-				else
+			case 'm':
+				if (floor(n1) != n1 || floor(n2) != n2)
 				{
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
-					cout << "Please Enter a Valid operation like + , -, *, / , m, s, etc ...\n";
+					cout << "Modulus operation requires integers!\n";
+					continue;
 				}
-			}
-
-			if (op == '+' || op == '-' || op == '*' || op == '/' || op == 'm')
-			{
-				cout << "Enter the first number: \n";
-				cin >> n1;
-				cout << "Enter the Seconed number: \n";
-				cin >> n2;
-			}
-			else if (op == 'k' || op == '.')
-			{
-				// Don not take any thing
-
-			}
-			else
-			{
-				cout << "Enter your Number: \n";
-				cin >> n1;
-			}
-			again = 1;
-			system("cls"); /////////////////////////////////////
-		}
-		switch (op)
-		{
-		case'+':
-			answer = n1 + n2;
-			break;
-		case'-':
-			answer = n1 - n2;
-			break;
-		case'*':
-			answer = n1 * n2;
-			break;
-		case'/':
-			if (n2 == 0)
-			{
-				cout << "Cannot Divide by Zero Bro !!!\n";
+				answer = int(n1) % int(n2);
+				break;
+			case'l':
+				answer = log(n1);
+				break;
+			case'S':
+				answer = n1 * n1;
+				break;
+			case'C':
+				answer = n1 * n1 * n1;
+				break;
+			case 'P':
+				double exponent;
+				cout << "Enter the exponent: \n";
+				cin >> exponent;
+				answer = pow(n1, exponent);
+				break;
+			case's':
+				answer = sin(n1);
+				break;
+			case'c':
+				answer = cos(n1);
+				break;
+			case't':
+				answer = tan(n1);
+				break;
+			case'r':
+				answer = sqrt(n1);
+				break;
+			case'k':
+				again = 0;
+				break;
+			case'.':
+				break;
+			default:
+				cout << "Invalid Operation bro! \n";
 				break;
 			}
-			else
-			{
-				answer = n1 / n2;
-				break;
-			}
-		case 'm':
-			if (floor(n1) != n1 || floor(n2) != n2) 
-			{
-				cout << "Modulus operation requires integers!\n";
-				continue;
-			}
-			answer = int(n1) % int(n2);
-			break;
-		case'l':
-			answer = log(n1);
-			break;
-		case'S':
-			answer = n1 * n1;
-			break;
-		case'C':
-			answer = n1 * n1 * n1;
-			break;
-		case 'P':
-			double exponent;
-			cout << "Enter the exponent: \n";
-			cin >> exponent;
-			answer = pow(n1, exponent);
-			break;
-		case's':
-			answer = sin(n1);
-			break;
-		case'c':
-			answer = cos(n1);
-			break;
-		case't':
-			answer = tan(n1);
-			break;
-		case'r':
-			answer = sqrt(n1);
-			break;
-		case'k':
-			again = 0;
-			break;
-		case'.':
-			break;
-		default:
-			cout << "Invalid Operation bro! \n";
-			break;
 		}
+	}
+	else
+	{
+
 	}
 	return 0;
 
